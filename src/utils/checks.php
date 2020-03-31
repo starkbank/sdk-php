37b4cd6c208e8750fda0241216ba17b4a82839fa
Caio Dottori <caio.dottori@starkbank.com>
2020-03-31 19:59:38 -0300

    added utils/checks

---
+++ src/utils/checks.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace StarkBank\Utils;
+use EllipticCurve\PrivateKey;
+use \Exception;
+use \DateTime;
+
+
+class Checks
+{
+    public static function checkEnvironment($environment)
+    {
+        assert(Environment::isValid($environment), "Select a valid environment:  " . strval(Environment::values()));
+        return $environment;
+    }
+
+    public static function checkPrivateKey($pem)
+    {
+        try {
+            PrivateKey::fromPem($pem);
+        } catch (Exception $e) {
+            throw new Exception("Private-key must be valid secp256k1 ECDSA string in pem format");
+        }
+        return $pem;
+    }
+
+    public static function checkDateTime($data)
+    {
+        if (is_null($data)){
+            return $data;
+        }
+
+        if ($data instanceof DateTime) {
+            return $data;
+        }
+
+        return new DateTime(strval($data));
+    }
+}
