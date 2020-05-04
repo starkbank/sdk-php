192823fd3ad8f7568d5645d69f8b761cc7191404
Caio Dottori <caio.dottori@starkbank.com>
2020-05-04 16:22:23 -0300

    version 0.3.0

--- src/utils/request.php
+++ src/utils/request.php
@@ -59,7 +59,7 @@ class Request
             "Access-Time" => $accessTime,
             "Access-Id" => $user->accessId(),
             "Access-Signature" => $signature,
-            "User-Agent" => "PHP-" . phpversion() . "-SDK-0.2.0",
+            "User-Agent" => "PHP-" . phpversion() . "-SDK-0.3.0",
             "Content-Type" => "application/json"
         ];
 
