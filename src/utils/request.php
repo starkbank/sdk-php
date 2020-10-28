e347cf6b5c6ed2e79245d6830afbefaffad88464
Caio Dottori <caio.dottori@starkbank.com>
2020-10-28 18:22:57 -0300

    Update version to v2.1.0

--- src/utils/request.php
+++ src/utils/request.php
@@ -59,7 +59,7 @@ class Request
             "Access-Time" => $accessTime,
             "Access-Id" => $user->accessId(),
             "Access-Signature" => $signature,
-            "User-Agent" => "PHP-" . phpversion() . "-SDK-2.0.0",
+            "User-Agent" => "PHP-" . phpversion() . "-SDK-2.1.0",
             "Content-Type" => "application/json",
             "Accept-Language" => Settings::getLanguage()
         ];
